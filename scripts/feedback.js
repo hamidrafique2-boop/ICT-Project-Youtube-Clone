378791366c0c0681b77db1eecfabf3ab62c5dc31
hamidrafique2-boop <hamidrafique2@gmail.com>
2025-12-21 23:37:55 +0500

    Update feedback.js

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -2,5 +2,5 @@ function submitFeedback(event) {
     if (event) event.preventDefault();
     const name = document.getElementById("name").value;
     alert(`Thank you, ${name}! Your feedback has been received.`);
-    window.location.href = "index.html";
+    window.location.href = "../index.html";
 }
